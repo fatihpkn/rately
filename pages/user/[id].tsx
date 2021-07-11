4c190c2383a3e50aac1119f6cd8b7f8a712b2f61
Fatih PKN <fatih.piskin@mtekbilisim.com>
2021-07-12 00:53:33 +0300

    :fire: :bug: remove useQuery for get employee

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,7 +1,7 @@
 import { ApolloQueryResult } from "@apollo/client";
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { QueryClient, useQuery, useQueryClient } from "react-query";
+import { QueryClient, useQueryClient } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import EmployeeProfile from "../../components/employee-profile";
 import MainContainer from "../../components/main-container";
@@ -19,12 +19,10 @@ const UserProfile: NextPage<IUserProfilePageProps> = (props) => {
 
   const client = useQueryClient();
 
-  const { data } = useQuery(["employee", params.id], async () => GetEmployeeByID(params.id));
-
   const _allEmployees = client.getQueryData<ApolloQueryResult<EmployeesQueryModel>>("employees");
   const _employee = client.getQueryData<ApolloQueryResult<EmployeeQueryModel>>(["employee", params.id]);
 
-  const employee = _allEmployees?.data.employees.find((em) => em.id === params.id) || data?.data?.employee;
+  const employee = _allEmployees?.data.employees.find((em) => em.id === params.id) || _employee?.data.employee;
 
   return (
     <div>
