befd330bd93bbad6845eaa81e2b8fed5662fb75f
Fatih PKN <fatih.piskin@mtekbilisim.com>
2021-07-27 14:26:13 +0300

    update api url https

--- apollo/index.ts
+++ apollo/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const Client = new ApolloClient({
-  uri: "http://whttmp.asuscomm.com/rately-api/graphql",
+  uri: "https://whttmp.asuscomm.com/rately-api/graphql",
   cache: new InMemoryCache(),
   ssrMode: true,
 });
